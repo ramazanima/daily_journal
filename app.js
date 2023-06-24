05ca0133f14475f983d00843fcb7318799476dbb
Ali Ramazani <ramazanim@berea.edu>
2023-06-24 19:01:05 -0400

    added compose page

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
+
 app.get("/", function(req, res){
   res.render("home", {homeContent: homeStartingContent});
+
 }); 
 
 app.get("/about", function(req, res){
@@ -30,6 +34,22 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactParagraph: contactContent});
 });
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+
+app.post("/compose", function(req, res){
+    const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+  posts.push(post);
+
+  res.redirect("/");
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
